0ab2000a0fdc9debd60470a67dd2d202b4c69ccc
Melissa Patterson <melissa.patterson.va@gmail.com>
2015-03-31 12:28:11 -0500

    migrate city users table
    forgot to migrate the city users table… did that this time

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331161229) do
+ActiveRecord::Schema.define(version: 20150331171306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 20150331161229) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "city_users", force: :cascade do |t|
+    t.integer  "city_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "city_users", ["city_id"], name: "index_city_users_on_city_id", using: :btree
+  add_index "city_users", ["user_id"], name: "index_city_users_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
@@ -33,4 +43,6 @@ ActiveRecord::Schema.define(version: 20150331161229) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "city_users", "cities"
+  add_foreign_key "city_users", "users"
 end
