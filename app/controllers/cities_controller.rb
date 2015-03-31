a95ddf8a6aecb09a49df65dadd45dfb4988d47ad
Melissa Patterson <melissa.patterson.va@gmail.com>
2015-03-31 14:04:43 -0500

    generate cities controller
    made cities controller with a method that returns all of the cities for
    a particular state in son

---
+++ app/controllers/cities_controller.rb
@@ -0,0 +1,12 @@
+class CitiesController < ApplicationController
+  
+  def show
+    @cities = City.where(state: params[:state].upcase)
+    render json: @cities, status: 200
+  end
+  
+  private
+  def state_params
+    params.require(:state)
+  end
+end
