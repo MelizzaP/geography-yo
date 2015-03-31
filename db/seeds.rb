b5807824d86911f190baf6cf408ae872de8e3989
Melissa Patterson <melissa.patterson.va@gmail.com>
2015-03-31 11:57:08 -0500

    seed for db
    this is a script that populates the db with the cities and users files
    when you run bundle exec rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+cities_csv_text = File.read('db/cities.csv')
+cities_csv = CSV.parse(cities_csv_text, :headers => true)
+
+cities_csv.each do |row|
+  City.create!(row.to_hash)
+end
+
+users_csv_text = File.read('db/users.csv')
+users_csv = CSV.parse(users_csv_text, :headers => true)
+
+users_csv.each do |row|
+  User.create!(row.to_hash)
+end
