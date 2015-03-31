61cdaeceaf27bd29285d2c579c83a4171e939a50
Melissa Patterson <melissa.patterson.va@gmail.com>
2015-03-31 17:14:45 -0500

    logic for 100 mile radius city query
    this is the logic for the 100 mile radius city query.  I converted all
    of the lat lon coords to radians and used a formula I derived from this
    wikipedia article http://en.wikipedia.org/wiki/Great-circle_distance.
    It assumes the earth is a sphere and that the radius is 4k miles, so
    it’s crazy accurate, but I think it works for this instance.  The logic
    in this controller is starting to get a little too much logic in it.  I
    might move this to the model.

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -6,11 +6,28 @@ class CitiesController < ApplicationController
   end
   
   def index
-    nearby_cities = City.where(
-    'name = ? AND state = ?',
-    params[:city].capitalize,
-    params[:state].upcase
-    )
+    selected_city = City.where(
+      'name = ? AND state = ?',
+      params[:city].capitalize,
+      params[:state].upcase
+      )
+    
+    lat = selected_city.first.latitude * Math::PI / 180
+    lon = selected_city.first.longitude * Math::PI / 180
+    
+    sql = %{
+      SELECT * 
+      FROM cities 
+      WHERE acos(
+        sin(#{lat}) * 
+        sin(radians(latitude)) + 
+        cos(#{lat}) * 
+        cos(radians(latitude)) * 
+        cos(radians(longitude) - (#{lon}))
+      ) * 4000 <= 100; 
+     } 
+         
+    nearby_cities = City.find_by_sql(sql)
     render json: nearby_cities, status: 200
   end
   
