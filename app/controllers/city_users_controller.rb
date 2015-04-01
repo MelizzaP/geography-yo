e60d98ec29aefca78a75dce08cb107ddbd5432de
Melissa Patterson <melissa.patterson.va@gmail.com>
2015-03-31 23:19:32 -0500

    error handling for invalid params
    this renders an error if the user and or city does not appear in the
    database

--- app/controllers/city_users_controller.rb
+++ app/controllers/city_users_controller.rb
@@ -2,18 +2,21 @@ class CityUsersController < ApplicationController
   skip_before_filter  :verify_authenticity_token
   
   def new
-    user = User.find(params[:user])
+    user = User.find_by(id: params[:user])
     city =  City.where(
       'name = ? AND state = ?',
       params[:city].capitalize,
       params[:state].upcase
     )
-   
+    if(user && city.length > 0)
       visit = CityUser.new
       visit.city_id = city.first.id
       visit.user_id = user.id
       visit.save
       render json: visit, status: 201
+    else
+      render json: 'User and/or city not in database :-(', status: 400
+    end
   end
 
   def index
