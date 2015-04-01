57c302deb3990faf889fbb3a2179f58849589de9
Melissa Patterson <melissa.patterson.va@gmail.com>
2015-03-31 23:17:00 -0500

    rm missing template logic
    moved error handling to controllers, because there aren’t too many
    scenarios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
-  rescue_from ActionView::MissingTemplate do |exception|
-    render json: "Your request needs to have a numerical value for it's radius", status: 400
-  end
 end
