145b24e69d1d8744cf719485fef22887a9d4f602
Melissa Patterson <melissa.patterson.va@gmail.com>
2015-03-31 21:19:07 -0500

    pagination for cities endpoint
    the cities endpoint now paginates per 50 results, to get more results
    simply add ‘&page=X’ to endpoint

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,6 +2,7 @@ class CitiesController < ApplicationController
   
   def show
     cities = City.where(state: params[:state].upcase)
+      .page(params[:page]).per(50)
     render json: cities, status: 200
   end
   
