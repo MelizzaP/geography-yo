f0449ca46c5b9d8d14add5fee8f1ec613ebbbd94
Melissa Patterson <melissa.patterson.va@gmail.com>
2015-03-31 21:20:00 -0500

    pagination for city radius endpoint
    similar to the cities endpoint, this one limits to 25 results per page

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,14 +3,14 @@ class City < ActiveRecord::Base
   has_many :users, through: :city_users
   
   def self.radius_search(params)
-    selected_city = City.where(
+    city = City.where(
       'name = ? AND state = ?',
       params[:city].capitalize,
       params[:state].upcase
     )
       
-    lat = selected_city.first.latitude * Math::PI / 180
-    lon = selected_city.first.longitude * Math::PI / 180
+    lat = city.first.latitude * Math::PI / 180
+    lon = city.first.longitude * Math::PI / 180
     
     sql = %{
       SELECT * 
@@ -23,7 +23,7 @@ class City < ActiveRecord::Base
         cos(radians(longitude) - (#{lon}))
       ) * 4000 <= #{params[:radius]}; 
      }
-     nearby_cities = City.find_by_sql(sql)
-     return nearby_cities 
+     radius = City.find_by_sql(sql)
+     return Kaminari.paginate_array(radius).page(params[:page])
   end
 end
