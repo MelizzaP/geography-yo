90af513a08441bf6072b62c4e85f3c88851eea3c
Melissa Patterson <melissa.patterson.va@gmail.com>
2015-03-31 19:34:20 -0500

    controller for new city user
    this is the controller action for a new user.  I’m skipping the rails
    standard verify authenticity token so that api requests can be made
    from anywhere i.e. postman.  this method takes a user id and city and
    adds it to the city user table.  no error handling yet

--- app/controllers/city_users_controller.rb
+++ app/controllers/city_users_controller.rb
@@ -1,5 +1,19 @@
 class CityUsersController < ApplicationController
-  def update
+  skip_before_filter  :verify_authenticity_token
+  
+  def new
+    user = User.find(params[:user])
+    city =  City.where(
+      'name = ? AND state = ?',
+      params[:city].capitalize,
+      params[:state].upcase
+    )
+   
+      visit = CityUser.new
+      visit.city_id = city.first.id
+      visit.user_id = user.id
+      visit.save
+      render json: visit, status: 201
   end
 
   def index
